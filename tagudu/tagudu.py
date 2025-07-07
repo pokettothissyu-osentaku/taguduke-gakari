65a8dec024087197c277e6de14af2c8a38b689ca
pokettothissyu-osentaku <pokettothissyu-osentaku@outlook.jp>
2025-07-07 20:50:31 +0900

    英語わからん

--- tagudu/tagudu.py
+++ tagudu/tagudu.py
@@ -5,6 +5,8 @@ from .tagged_directory import TaggedDirectory
 
 
 def main():
+    """コマンド定義"""
+
     fire.Fire(
         {
             "set": set,
@@ -19,49 +21,97 @@ def main():
     )
 
 
-def set(filename, *tags):
+def set(filename: str, *tags: str):
+    """ファイルに対してタグを設定
+    
+    Args:
+        filename (str): ファイル名。未登録のものも指定可
+        tags (str): タグのリスト。設定済みのものも指定可
+
+    """
+
     tagged_directory = TaggedDirectory("./")
     tagged_directory.set_tags(filename, list(tags))
     tagged_directory.save_json()
 
 
-def remove(filename, *tags):
+def remove(filename: str, *tags: str):
+    """ファイルからタグを解除
+    
+    Args:
+        filename (str): ファイル名
+        tags (str): タグのリスト
+
+    """
+
     tagged_directory = TaggedDirectory("./")
     tagged_directory.remove_tags(filename, list(tags))
     tagged_directory.save_json()
 
 
-def tags(filename):
+def tags(filename: str):
+    """ファイルに設定されているタグのリストを表示
+
+    Args:
+        filename (str): ファイル名
+    
+    """
+
     tagged_directory = TaggedDirectory("./")
     for tag in tagged_directory.get_tag_list(filename):
         print(tag)
 
 
 def counts():
+    """タグ数の集計を表示"""
+
     tagged_directory = TaggedDirectory("./")
     for tag, num in tagged_directory.count_tags().items():
         print(f"{tag}: {num}")
 
 
-def filter_(*tags):
+def filter_(*tags: str):
+    """ファイルを絞り込む。指定されたタグのいずれかを含むファイルを対象とする（ORモード）
+    
+    Args:
+        tags (str): タグのリスト
+    
+    """
+
     tagged_directory = TaggedDirectory("./")
     for filename in tagged_directory.filter_by_tags(list(tags)):
         print(filename)
 
 
-def filter_and(*tags):
+def filter_and(*tags: str):
+    """ファイルを絞り込む。指定されたタグをすべて含むファイルを対象とする（ANDモード）
+    
+    Args:
+        tags (str): タグのリスト
+    
+    """
+    
     tagged_directory = TaggedDirectory("./")
     for filename in tagged_directory.filter_by_tags(list(tags), mode="and"):
         print(filename)
 
 
-def filter_re(pattern):
+def filter_re(pattern: str):
+    """正規表現によりファイルを絞り込む
+
+    Args:
+        pattern (str): 正規表現
+    
+    """
+
     tagged_directory = TaggedDirectory("./")
     for filename in tagged_directory.filter_by_regular_expression(pattern):
         print(filename)
 
 
 def reset():
+    """ファイルの配置をリセットする"""
+
     tagged_directory = TaggedDirectory("./")
     tagged_directory.reset_directory_structure()
 
